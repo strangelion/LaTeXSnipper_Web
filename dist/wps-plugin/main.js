fef7bbf3c7a4c591ab7ef4efa9c5f26d0294dce5
strangelion <122724551+strangelion@users.noreply.github.com>
2026-06-12 23:56:38 +0800

    清除文件

--- dist/wps-plugin/main.js
+++
@@ -1,6 +0,0 @@
-// LaTeXSnipper WPS Plugin - Main Entry
-// Follows WPS JSAddIn pattern from gitee.com/zouyf/wps
-
-var time = new Date().getTime();
-document.write("<script language='javascript' src='js/util.js?time=" + time + "'></script>");
-document.write("<script language='javascript' src='js/ribbon.js?time=" + time + "'></script>");
